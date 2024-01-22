58858ad260951227bb7156983aed3d27b0c823dc
Erika Fernandes <erikafernandes50@outlook.com>
2024-01-22 17:36:11 -0300

    exercicio Ebac bank

--- HTML-JS/main.js
+++ HTML-JS/main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false;
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,24 +8,35 @@ function validaNome(nomeCompleto) {
 }
 
 form.addEventListener('submit' ,function(e) {
-    let formEValido = false;
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario  = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${valorDeposito.value} - conta: ${numeroContaBeneficiario.value}`;
-
-    formEValido = validaNome(nomeBeneficiario.value)
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${valorDeposito.value} - conta: ${numeroContaBeneficiario}`;
+    formEValido = validaNome(nomeBeneficiario.value)    
     if (formEValido) {
-        alert(mensagemSucesso); 
-
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';    
+        
         nomeBeneficiario.value = ' ';
         numeroContaBeneficiario.value = ' ';
         valorDeposito.value = ' ';
-        } else {
-            alert("O nome não está completo!");
-        }
-})
-console.log(form);
+    } else {
+        nomeBeneficiario.sytle.border = '1pix solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})    
 
+nomeBeneficiario.addEventListener('keyup', function(e) {
+        console.log(e.target.value);
+        formEValido = validaNome(e.target.value);
+
+        if (!formEValido) {
+            nomeBeneficiario.classList.add('error');
+            document.querySelector('.error-message').style.display = 'block';
+        } else {
+            nomeBeneficiario.classList.remove('error');
+            document.querySelector('.error-message').style.display = 'none';
+        }
+    })
